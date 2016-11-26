73779a32d6489bed3d66b4f88e6f74c4e93a9271
Theo96sc <theo_96.sc@outlook.es>
2016-11-26 13:07:50 -0500

    actualizacion 1

--- ProyectoHospital01/clsHistClinica.cs
+++ ProyectoHospital01/clsHistClinica.cs
@@ -8,5 +8,26 @@ namespace ProyectoHospital01
 {
     class clsHistClinica
     {
+        private string no_HistCl { get; set; }
+        private string obsGenerales { get; set; }
+        private string sintomas { get; set; }
+        private decimal peso { get; set; }
+        private decimal temperatura { get; set; }
+        private decimal altura { get; set; }
+        private string diagnostico { get; set; }
+        private string concluMedicas { get; set; }
+
+        public clsHistClinica()
+        {
+            no_HistCl = "";
+            obsGenerales = "";
+            sintomas = "";
+            peso = 0;
+            temperatura = 0;
+            altura = 0;
+            diagnostico = "";
+            concluMedicas = "";
+
+        }
     }
 }
