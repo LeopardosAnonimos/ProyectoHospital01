57cd61d9af0c14ca57cc32d9386fe6bda292ab91
JeffVelasquez <jeffervelasquez@hotmail.com>
2016-11-26 12:45:30 -0500

    MedicoModi

--- ProyectoHospital01/clsMedico.cs
+++ ProyectoHospital01/clsMedico.cs
@@ -8,5 +8,19 @@ namespace ProyectoHospital01
 {
     class clsMedico
     {
+        private String especialidad { get; set; }
+
+        public clsMedico(String nombre, String apellido, String direccion, String id, String telefono, char sexo, int edad,
+            DateTime fechNac, String pswd)
+            : base(nombre, apellido, direccion, id, telefono, sexo, edad, fechNac, pswd)
+        {
+
+        }
+
+        public clsMedico(String nombre, String apellido, String telefono, String especialidad)
+            : base(nombre, apellido, telefono)
+        {
+            this.especialidad = especialidad;
+        }
     }
 }
