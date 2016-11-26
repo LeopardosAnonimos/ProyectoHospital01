3ad9bc2e14b17200cf01a3ad75f4ce08c0bbb510
JeffVelasquez <jeffervelasquez@hotmail.com>
2016-11-26 13:40:15 -0500

    PruebaUpdate

--- ProyectoHospital01/clsFuncionario.cs
+++ ProyectoHospital01/clsFuncionario.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//pinche theo mamón
 namespace ProyectoHospital01
 {
     class clsFuncionario:clsPersona
