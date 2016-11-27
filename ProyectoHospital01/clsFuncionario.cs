45bdf783991330d8b8cedea2d27724486615de62
Theo96sc <theo_96.sc@outlook.es>
2016-11-26 19:40:26 -0500

    sunc

--- ProyectoHospital01/clsFuncionario.cs
+++ ProyectoHospital01/clsFuncionario.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//pinche theo mamón
+
+
 namespace ProyectoHospital01
 {
     class clsFuncionario:clsPersona
